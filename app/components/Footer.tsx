59c0564a1c4d94fac00c101125a15dc0d60fdcda
BEST SMABoSch <bestsmaittbzbs@gmail.com>
2026-09-08 17:01:13 +0700

    Update Footer.tsx
    yt

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -102,7 +102,7 @@ export default function Footer() {
               </a>
 
               <a
-                href="mailto:info@sagatha-event.id"
+                href="mailto:bestsmaittbzbs@gmail.com"
                 className="flex items-center gap-3 p-3 rounded-2xl bg-[#0d2a2e] border border-white/10 hover:border-[#1FB873] transition group"
               >
                 <div className="w-9 h-9 rounded-xl bg-[#136368] text-white flex items-center justify-center border border-[#248999]">
