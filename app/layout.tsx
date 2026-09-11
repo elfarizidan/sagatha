b70e4ffc8696f87fb3163410e588a5f2b2a7c3e7
elfarizidan <elfarizidan@gmail.com>
2026-09-11 23:22:19 +0700

    Update metadata title and description in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SAGATHA IV | Saga Thariq IV",
+  title: "SAGATHA",
   description:
-    "Official website SAGATHA IV. Kompetisi Futsal, Speech, OSN Matematika, Puisi, Mural, dan Musabaqah Hifzhil Qur'an (MHQ). Total Hadiah Puluhan Juta Rupiah!",
+    "Official website SAGATHA",
 };
 
 export default function RootLayout({
