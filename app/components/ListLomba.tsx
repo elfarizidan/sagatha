3481fc95d1a4ffaaab01a628683700de8f6aa560
BEST SMABoSch <bestsmaittbzbs@gmail.com>
2026-09-16 20:36:42 +0700

    Update ListLomba.tsx

--- app/components/ListLomba.tsx
+++ app/components/ListLomba.tsx
@@ -63,7 +63,7 @@ export default function ListLomba() {
         "Melampirkan Kartu Pelajar aktif saat registrasi ulang",
       ],
       contactPerson: "via form",
-      LinkForm: "https://forms.gle/CN7nJcxAevBtesUt7",
+      WhatsappNumber: "https://forms.gle/CN7nJcxAevBtesUt7",
       driveUrl: "https://drive.google.com/drive/folders/1LBdHq-lXsdHh-S65wvRmdwgEZoLxC8Al?usp=drive_link",
     },
     {
