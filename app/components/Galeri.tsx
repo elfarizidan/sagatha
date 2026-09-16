38e0d790a256b012963c3068efaa5c214271f4a8
Zidan Alfarizi <elfarizidan@gmail.com>
2026-09-16 20:16:02 +0700

    Change image format from PNG to JPEG in gallery

--- app/components/Galeri.tsx
+++ app/components/Galeri.tsx
@@ -31,7 +31,7 @@ export default function Galeri() {
   const galleryItems: GalleryItem[] = [
     {
       id: 1,
-      src: "/content/gallery-1.png",
+      src: "/content/gallery-1.jpeg",
       title: "Semarak Panitia",
       desc: "Semangatnya panitia SAGATHA dalam menyelenggarakan dan menyambut seluruh peserta.",
     },
