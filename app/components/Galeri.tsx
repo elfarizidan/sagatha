3dd3f394f5fe66410de09bf46e1ff89b119202ad
BEST SMABoSch <bestsmaittbzbs@gmail.com>
2026-09-16 21:21:50 +0700

    Update Galeri.tsx

--- app/components/Galeri.tsx
+++ app/components/Galeri.tsx
@@ -31,7 +31,7 @@ export default function Galeri() {
   const galleryItems: GalleryItem[] = [
     {
       id: 1,
-      src: "/content/gallery-1.jpg",
+      src: "/content/IMG_9784.jpg",
       title: "Semarak Panitia",
       desc: "Semangatnya panitia SAGATHA dalam menyelenggarakan dan menyambut seluruh peserta.",
     },
