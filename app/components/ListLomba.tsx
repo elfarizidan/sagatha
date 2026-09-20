cd04f12db74e8061b89b04ddc0970dfbd50040fe
BEST SMABoSch <bestsmaittbzbs@gmail.com>
2026-09-20 11:44:08 +0700

    Update ListLomba.tsx

--- app/components/ListLomba.tsx
+++ app/components/ListLomba.tsx
@@ -195,7 +195,7 @@ export default function ListLomba() {
       registrationUrl: "https://forms.gle/2cNpkirviAxKGtat8",
       driveUrl: "https://drive.google.com/drive/folders/1LBdHq-lXsdHh-S65wvRmdwgEZoLxC8Al?usp=drive_link",
     },
-],
+];
     
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
